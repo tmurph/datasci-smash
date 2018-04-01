1a1b81c5710e64cca784212022b6a28f39382ddf
Trevor Murphy <trevor.m.murphy@gmail.com>
2018-03-31 22:01:31 -0700

    parallelize the fucking recording!
    holy shit, I can give it a different user directory.  gotta populate it
    with all my stuff, though.  so, yeah, this recording script is
    suuuuuuuuuper unique to my setup, ugh.

--- scripts/record_avi.sh
+++ scripts/record_avi.sh
@@ -6,18 +6,25 @@ data_dir="$(dirname $0)/record_avi"
 melee_iso="${data_dir}/Super Smash Bros. Melee (v1.02).iso"
 
 dolphin=/Applications/Dolphin.app/Contents/MacOS/Dolphin
-dolphin_dir=/Users/trevor/Library/Application\ Support/Dolphin/Dump/Frames
-dumb_video="${dolphin_dir}/framedump0.avi"
-dolphin_video="${dolphin_dir}/framedump1.avi"
+
+user_dir=/Users/trevor/Library/Application\ Support/Dolphin
+tmp_dir=$(mktemp -d 2>/dev/null || mktemp -d -t 'dolphin')
+cp -r -t "$tmp_dir" "$user_dir"
+tmp_user_dir="${tmp_dir}/Dolphin"
+
+frame_dir="${tmp_user_dir}/Dump/Frames"
+dumb_video="${frame_dir}/framedump0.avi"
+dolphin_video="${frame_dir}/framedump1.avi"
 
 rm -f "$dumb_video" "$dolphin_video"
 
 # On my Mac OSX, Dolphin --batch still requires user input to shut down
 # when the movie finishes :(.  Let's try to hack around it with dumb
 # sleeping mechanisms.
-"$dolphin" -e "$melee_iso" -b -m "$dtm_filename" &
+"$dolphin" -u "$tmp_user_dir" -e "$melee_iso" -b -m "$dtm_filename" &
 dolphin_pid=$!
 read -p "Sleep for: $total_sec" -t $total_sec
 kill -TERM $dolphin_pid
 
 mv "$dolphin_video" "$avi_filename"
+rm -rf "$tmp_dir"
