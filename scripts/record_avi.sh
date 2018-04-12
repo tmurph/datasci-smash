4c4f48bf913c91e9f55474720228e90a044cd85b
Trevor Murphy <trevor.m.murphy@gmail.com>
2018-04-12 00:10:06 +0000

    fixup stupid record avi mistakes

--- scripts/record_avi.sh
+++ scripts/record_avi.sh
@@ -8,8 +8,6 @@ melee_iso="${avi_dir}/Super Smash Bros. Melee (v1.02).iso"
 dolphin=/usr/games/dolphin-emu
 user_dir="${avi_dir}/dolphin-user"
 
-user_dir_base=$(basename "$user_dir")
-
 frame_dir="${user_dir}/Dump/Frames"
 dumb_video="${frame_dir}/framedump0.avi"
 dolphin_video="${frame_dir}/framedump1.avi"
@@ -19,10 +17,9 @@ rm -f "$dumb_video" "$dolphin_video"
 # On my Mac OSX, Dolphin --batch still requires user input to shut down
 # when the movie finishes :(.  Let's try to hack around it with dumb
 # sleeping mechanisms.
-"$dolphin" -u "$tmp_user_dir" -e "$melee_iso" -b -m "$dtm_filename" &
+"$dolphin" -u "$user_dir" -e "$melee_iso" -b -m "$dtm_filename" &
 dolphin_pid=$!
 read -p "Sleep for: $total_sec" -t $total_sec
 kill -TERM $dolphin_pid
 
 mv "$dolphin_video" "$avi_filename"
-rm -rf "$tmp_dir"
