1a8be35c9dbe3f680663ff7c2814a802f3628a93
Trevor Murphy <trevor.m.murphy@gmail.com>
2018-04-12 00:10:38 +0000

    only use one dolphin framedump video
    this is actually really subtle.  earlier versions of dolphin would dump
    all frames to one file.  later versions of dolphin (including v5, the
    one I have to use to get melee cheats) will create a new framedump video
    each time the screen resizes.
    
    I was getting two videos forever, because my settings resized the video
    frame.  so dolphin would load up the default frame, create a file,
    resize the frame, and immediately skip to the next file to start dumping
    frames.
    
    the config folder I've uploaded for this project is as close as I can
    get to a fresh dolphin config.  in particular, it doesn't resize the
    frame at all.  so it only uses the one file.

--- scripts/record_avi.sh
+++ scripts/record_avi.sh
@@ -9,10 +9,9 @@ dolphin=/usr/games/dolphin-emu
 user_dir="${avi_dir}/dolphin-user"
 
 frame_dir="${user_dir}/Dump/Frames"
-dumb_video="${frame_dir}/framedump0.avi"
-dolphin_video="${frame_dir}/framedump1.avi"
+dolphin_video="${frame_dir}/framedump0.avi"
 
-rm -f "$dumb_video" "$dolphin_video"
+rm -f "$dolphin_video"
 
 # On my Mac OSX, Dolphin --batch still requires user input to shut down
 # when the movie finishes :(.  Let's try to hack around it with dumb
